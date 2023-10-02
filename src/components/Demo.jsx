929a4856e9f4db04643ea7666c9652977e4f26de
TeeAkinlade <taiye.akinlade1@gmail.com>
2023-10-02 18:20:00 +0100

    changes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -106,7 +106,7 @@ const Demo = () => {
                     <div className="flex flex-col gap-3">
                         <h2 className="font-satoshi font-bold text-gray-600 text-xl">
                             Article 
-                            <span className='blue_gradient'>Summary</span>
+                            <span className='blue_gradient ml-2'>Summary</span>
                         </h2>
                         <div className="summary_box">
                             <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
